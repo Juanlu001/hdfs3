b004f0d53c76ee0aaf0521e9fcf0b9eb5bd71cd1
Juan Luis Cano Rodríguez <juanlu001@gmail.com>
2016-01-12 21:33:52 +0100

    Alternate approach to finding the library

--- hdfs3/lib.py
+++ hdfs3/lib.py
@@ -19,14 +19,7 @@ This is brittle both to changes in conda info and in cases where users don't
 install libhdfs3 through conda.
 """
 
-proc = subprocess.Popen(['conda', 'info'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-out, err = proc.communicate()
-conda_directory = next(line for line in out.split(b'\n')
-            if "default environment" in line.decode())
-conda_directory = conda_directory.decode().split(':')[-1].strip()
-so_directory = os.path.join(conda_directory, 'lib')
-
-_lib = ct.cdll.LoadLibrary(os.sep.join([so_directory, 'libhdfs3.so']))
+_lib = ct.cdll.LoadLibrary(ctypes.util.find_library('libhdfs3.so') or os.path.join(sys.prefix, 'lib', 'libhdfs3.so'))
 
 
 tSize = ct.c_int32
